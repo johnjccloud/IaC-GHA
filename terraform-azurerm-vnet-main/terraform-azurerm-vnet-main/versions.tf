5bdfe37ef6b94a5ff8150835a8adbd3c88409c10
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-08-29 17:08:08 +0100

    verz

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/versions.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/versions.tf
@@ -5,7 +5,7 @@ terraform {
     required_providers {
         azurerm = {
             source = "hashicorp/azurerm"
-            version = "~> 1.3.5"
+            version = "~> 2.0"
         }
     }
     backend "azurerm" {
