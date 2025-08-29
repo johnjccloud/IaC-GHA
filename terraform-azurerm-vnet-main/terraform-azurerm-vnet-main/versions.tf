40104e27d662d7e1d6ccedf159e57ff25beb8a84
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-08-29 17:12:16 +0100

    terrfile

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/versions.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/versions.tf
@@ -1,23 +1,18 @@
-##################################################################################
-# TERRAFORM CONFIG
-##################################################################################
 terraform {
-    required_providers {
-        azurerm = {
-            source = "hashicorp/azurerm"
-            version = "~> 2.0"
-        }
+  required_version = ">= 1.3.5"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
     }
-    backend "azurerm" {
-        key = "app.terraform.tfstate"
+    modtm = {
+      source  = "Azure/modtm"
+      version = "0.3.2"
     }
-}
-
-
-##################################################################################
-# PROVIDERS
-##################################################################################
-
-provider "azurerm" {
-  features {}
+    random = {
+      source  = "hashicorp/random"
+      version = ">= 3.3.2, < 4.0"
+    }
+  }
 }
