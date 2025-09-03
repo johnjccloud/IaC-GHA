1590af50773247c9e48803f5945b6e7d859b50a8
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-09-03 13:48:04 +0100

    add-nsg-7

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/versions.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/versions.tf
@@ -19,3 +19,12 @@ terraform {
 provider "azurerm" {
   features {}
 }
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.32.0"
+    }
+  }
+}
