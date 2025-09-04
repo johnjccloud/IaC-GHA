09407e7b18c6a527ea9ebcaf407d6d0987421789
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-09-04 10:50:38 +0100

    motdm

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/terraform.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/terraform.tf
@@ -8,7 +8,11 @@ terraform {
             source = "hashicorp/azurerm"
             version = "~> 2.0"
         }
-    }
+        modtm = {
+          source  = "Azure/modtm"
+          version = "0.3.2"
+        }
+      }    
     backend "azurerm" {
         key = "app.terraform.tfstate"
     }
