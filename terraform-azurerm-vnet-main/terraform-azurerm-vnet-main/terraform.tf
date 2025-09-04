b772ffaf9ba218e370b8227a6125b289a47a7915
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-09-04 10:33:43 +0100

    tf version

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/terraform.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/terraform.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 4.0"
+      version = "~> 2.0"
     }
     modtm = {
       source  = "Azure/modtm"
