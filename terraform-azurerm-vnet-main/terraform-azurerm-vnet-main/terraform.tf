a8c0b1affd9a9ebb973831e6db511410594490b0
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-09-04 12:55:55 +0100

    tf ver

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/terraform.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/terraform.tf
@@ -2,7 +2,7 @@ terraform {
     required_providers {
         azurerm = {
             source = "hashicorp/azurerm"
-            version = "~> 2.0"
+            version = "~> 2.99"
         }
         modtm = {
           source  = "Azure/modtm"
@@ -15,8 +15,5 @@ terraform {
     }
 }
 provider "azurerm" {
-  subscription_id = "58c4307d-1129-4116-a1e7-782415146818"
-  client_id = "90367a60-e3ea-4618-9953-e85ce291ff39"
-  tenant_id = "fa97859f-1a91-4ae1-ba3e-05a73b452efd"
   features {}
 }
