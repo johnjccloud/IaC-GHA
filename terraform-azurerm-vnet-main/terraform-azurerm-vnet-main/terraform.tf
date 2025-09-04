db732078429a9a87ea14b2f23d3274da64edd7e1
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-09-04 12:52:21 +0100

    push7

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/terraform.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/terraform.tf
@@ -15,8 +15,8 @@ terraform {
     }
 }
 provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id = var.client_id
-  tenant_id = var.tenant_id
+  subscription_id = "58c4307d-1129-4116-a1e7-782415146818"
+  client_id = "90367a60-e3ea-4618-9953-e85ce291ff39"
+  tenant_id = "fa97859f-1a91-4ae1-ba3e-05a73b452efd"
   features {}
 }
