13c6d4ff947850696ff8ffd5f75668cd625fdddc
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-09-04 11:59:27 +0100

    push

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/providers.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/providers.tf
@@ -1,6 +1,6 @@
 provider "azurerm" {   
     subscription_id = 58c4307d-1129-4116-a1e7-782415146818
-    client_id = 90367a60-e3ea-4618-9953-e85ce291ff39
-    tenant_id = fa97859f-1a91-4ae1-ba3e-05a73b452efd   
+    client_id = 90367a60-e3ea-4618-9953-e85ce291ff39 
+    tenant_id = fa97859f-1a91-4ae1-ba3e-05a73b452efd
     features {} 
     }
