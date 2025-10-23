a0de76f8c91bd8ed2719e7183c292ffd4578fd2d
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-10-23 13:04:20 +0100

    RDP rule

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/nsg.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/nsg.tf
@@ -36,6 +36,19 @@ resource "azurerm_network_security_group" "app-grp" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  
+ security_rule {
+    name                       = "Allow-RDP"
+    priority                   = 202
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
 resource "azurerm_subnet_network_security_group_association" "sg-appsubnet" {
   subnet_id = azurerm_subnet.appsubnet.id
