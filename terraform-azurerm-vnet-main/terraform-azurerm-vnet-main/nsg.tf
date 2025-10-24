5f037b82245fe8e9939f6ef38260727301f820b2
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-10-24 15:58:41 +0100

    any nsg rule

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/nsg.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/nsg.tf
@@ -62,6 +62,18 @@ resource "azurerm_network_security_group" "app-grp" {
     destination_address_prefix = "*"
   }
 
+   security_rule {
+    name                       = "Allow-Any"
+    priority                   = 203
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
 resource "azurerm_subnet_network_security_group_association" "sg-appsubnet" {
   subnet_id = azurerm_subnet.appsubnet.id
