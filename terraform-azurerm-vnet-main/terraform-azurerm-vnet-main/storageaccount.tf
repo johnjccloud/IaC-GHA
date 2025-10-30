5fb7d5bc9f0a5526dde1fbd944ebe7b5eafd989b
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-10-30 10:56:12 +0000

    storage and vaults

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/storageaccount.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/storageaccount.tf
@@ -6,3 +6,30 @@ resource "azurerm_storage_account" "example" {
   account_replication_type = "GRS"
 
   }
+
+resource "azurerm_storage_account" "example2" {
+  name                     = var.storageaccount2_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.vnet_location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  }
+
+resource "azurerm_storage_account" "example3" {
+  name                     = var.storageaccount3_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.vnet_location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  }
+
+  resource "azurerm_storage_account" "example4" {
+  name                     = var.storageaccount4_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.vnet_location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  }
