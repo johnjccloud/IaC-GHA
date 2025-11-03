5f2213cbd1cc1547d1671a97514c78993aa79bb7
johnjccloud <137486288+johnjccloud@users.noreply.github.com>
2025-11-03 11:45:16 +0000

    ftp

--- terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/nsg.tf
+++ terraform-azurerm-vnet-main/terraform-azurerm-vnet-main/nsg.tf
@@ -269,3 +269,17 @@ resource "azurerm_network_security_rule" "in6" {
   resource_group_name         = var.resource_group_name
   network_security_group_name = azurerm_network_security_group.test-grp2.name
 }
+
+resource "azurerm_network_security_rule" "in7" {
+  name                        = "Allow-FTP-VirtualNetwork"
+  priority                    = 210
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "23"
+  source_address_prefix       = "VirtualNetwork"
+  destination_address_prefix  = "VirtualNetwork"
+  resource_group_name         = var.resource_group_name
+  network_security_group_name = azurerm_network_security_group.test-grp2.name
+}
